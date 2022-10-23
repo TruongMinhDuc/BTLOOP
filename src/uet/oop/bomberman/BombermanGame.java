9c826090d6c4730f9bc28e66707e3614f57ef8ce
TruongMinhDuc <21020307@vnu.edu.vn>
2022-10-23 13:26:12 +0700

    title

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -55,7 +55,7 @@ public class BombermanGame extends Application {
         // Them scene vao stage
         stage.setScene(scene);
         stage.show();
-
+        stage.setTitle("Bomberman");
 
 
         AnimationTimer timer = new AnimationTimer() {
