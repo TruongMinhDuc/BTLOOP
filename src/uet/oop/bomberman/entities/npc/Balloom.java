c0b116ae28e3ea1fed15c89f4c4be9e8e444378f
TruongMinhDuc <21020307@vnu.edu.vn>
2022-10-26 21:11:44 +0700

    fix bug

--- src/uet/oop/bomberman/entities/npc/Balloom.java
+++ src/uet/oop/bomberman/entities/npc/Balloom.java
@@ -1,26 +1,28 @@
 package uet.oop.bomberman.entities.npc;
 
 import javafx.scene.image.Image;
+import uet.oop.bomberman.BombermanGame;
 import uet.oop.bomberman.graphics.Sprite;
 
 public class Balloom extends  Enemy{
+    int duration = 0;
     public Balloom(int xUnit, int yUnit, Image img, double speed) {
         super(xUnit, yUnit, img, speed);
-        setFrameRight();
-        setFrameLeft();
-        setFrameDie();
+        loadFrameRight();
+        loadFrameLeft();
+        loadFrameDie();
     }
-    public void setFrameRight() {
+    public void loadFrameRight() {
         frameRight[0] = Sprite.balloom_right1.getFxImage();
         frameRight[1] = Sprite.balloom_right2.getFxImage();
         frameRight[2] = Sprite.balloom_right3.getFxImage();
     }
-    public void setFrameLeft() {
+    public void loadFrameLeft() {
         frameLeft[0] = Sprite.balloom_left1.getFxImage();
         frameLeft[1] = Sprite.balloom_left2.getFxImage();
         frameLeft[2] = Sprite.balloom_left3.getFxImage();
     }
-    public void setFrameDie() {
+    public void loadFrameDie() {
         frameDie[0] = Sprite.balloom_dead.getFxImage();
         frameDie[1] = Sprite.mob_dead1.getFxImage();
         frameDie[2] = Sprite.mob_dead2.getFxImage();
@@ -33,6 +35,20 @@ public class Balloom extends  Enemy{
     }
     @Override
     public void dying() {
-
+        if (duration < 10) {
+            this.setImg(frameDie[0]);
+            duration++;
+        } else if (duration < 20) {
+            this.setImg(frameDie[1]);
+            duration++;
+        } else if (duration < 30) {
+            this.setImg(frameDie[2]);
+            duration++;
+        } else if (duration < 40) {
+            this.setImg(frameDie[3]);
+            //Board.score += 100;
+            BombermanGame.eventHandler.removeEnemyAt(this.x, this.y);
+        }
     }
+
 }
