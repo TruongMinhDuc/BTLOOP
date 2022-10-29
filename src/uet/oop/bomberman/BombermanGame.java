101133d4a2ac06445206ee2be3a1b476d3ef5c99
TruongMinhDuc <21020307@vnu.edu.vn>
2022-10-29 11:25:31 +0700

    show game status

--- src/uet/oop/bomberman/BombermanGame.java
+++ src/uet/oop/bomberman/BombermanGame.java
@@ -8,6 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import uet.oop.bomberman.entities.Bomb.Bomb;
 import uet.oop.bomberman.entities.character.Bomber;
@@ -30,6 +33,13 @@ public class BombermanGame extends Application {
     public static Controller controller;
     public static EventHandler eventHandler;
 
+
+    private Text playerLife;
+    private Text showLevel;
+    private Text showBomb;
+    private Text showScore;
+    private List<Text> textList = new ArrayList<>();
+
     public static void main(String[] args) {
         Application.launch(BombermanGame.class);
     }
@@ -47,38 +57,96 @@ public class BombermanGame extends Application {
         root.getChildren().add(canvas);
 
         // Tao scene
-        Scene scene = new Scene(root, 37 * Sprite.SCALED_SIZE, HEIGHT * Sprite.SCALED_SIZE, Color.BLACK );
+        Scene scene = new Scene(root, (WIDTH + 6) * Sprite.SCALED_SIZE, HEIGHT * Sprite.SCALED_SIZE, Color.GREY);
+
+        try {
+            newGame();
+            createBomberStatus();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
 
         // Them scene vao stage
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Bomberman");
 
-
+        root.getChildren().addAll(textList);
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-              eventHandler.render();
-              eventHandler.update();
+                update();
+                eventHandler.render();
+                eventHandler.update();
             }
         };
+
         timer.start();
-        try {
-            newGame();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+
+
         controller.handle(scene);
 
     }
 
     public void newGame() throws FileNotFoundException {
-            eventHandler = new EventHandler();
-            controller = new Controller();
-            eventHandler.getPlayer().setLife(3);
-            eventHandler.setLevel(1);
-            eventHandler.getGameLevel().createMap(1);
+        eventHandler = new EventHandler();
+        controller = new Controller();
+        eventHandler.getPlayer().setLife(3);
+        eventHandler.setLevel(1);
+        eventHandler.getGameLevel().createMap(1);
+        eventHandler.setShowLevel(eventHandler.getLevel());
     }
 
+    public void update() {
+        playerLife.setText(String.valueOf(eventHandler.getPlayer().getLife()));
+        showLevel.setText(String.valueOf(eventHandler.getShowLevel()));
+        showBomb.setText(String.valueOf(Bomb.maxBomb - Bomber.getBombsList().size()));
+        showScore.setText(String.valueOf(EventHandler.getScore()));
+    }
 
+    public void createBomberStatus() {
+        Font font = Font.font("System Bold", FontWeight.EXTRA_BOLD, 25);
+        Color color = Color.WHITE;
+// bomber life
+        Text lifeText = new Text(1020, 90, "Left: ");
+        lifeText.setFill(color);
+        lifeText.setFont(font);
+        textList.add(lifeText);
+
+        playerLife = new Text(1110, 90, String.valueOf(eventHandler.getPlayer().getLife()));
+        playerLife.setFill(color);
+        playerLife.setFont(font);
+        textList.add(playerLife);
+// Bomber level
+        Text levelText = new Text(1020, 30, "Level: ");
+        levelText.setFill(color);
+        levelText.setFont(font);
+        textList.add(levelText);
+
+        showLevel = new Text(1110, 30, String.valueOf(eventHandler.getShowLevel()));
+        showLevel.setFill(color);
+        showLevel.setFont(font);
+        textList.add(showLevel);
+// Bomber bomb
+        Text bombText = new Text(1020, 150, "Bomb: ");
+        bombText.setFill(color);
+        bombText.setFont(font);
+        textList.add(bombText);
+
+        showBomb = new Text(1110, 150, String.valueOf(Bomb.maxBomb - Bomber.getBombsList().size()));
+        showBomb.setFill(color);
+        showBomb.setFont(font);
+        textList.add(showBomb);
+// Bomber score
+        Text scoreText = new Text(1020, 210, "sore: ");
+        scoreText.setFill(color);
+        scoreText.setFont(font);
+        textList.add(scoreText);
+
+        showScore = new Text(1110, 210, String.valueOf(EventHandler.getScore()));
+        showScore.setFill(color);
+        showScore.setFont(font);
+        textList.add(showScore);
+    }
 }
